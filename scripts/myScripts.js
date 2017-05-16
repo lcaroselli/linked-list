25afa9f6e463157c9ad63f98831d40996b1b5377
Laura Caroselli <lcaroselli@outlook.com>
2017-05-15 18:23:09 -0600

    Create and append JS elements in section 2

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,57 +1,92 @@
 /*
-
-TODO:
-
+TO DO:
 need functions for:
-
 - array manipulation: add card to array, remove card from array, mark as read
 - dom manipulation:   loop through array and add each card to dom
                       destroy all cards
 - bookmark builder: a function that builds our html elements and applies the styling
-
-Questions:
-- is it a good habit to include a label control for inputs even if there is no text
-    the label control should only be used when an actual label exists for your input
-
-
 */
+
 
 // Global Variables
 
 var userDisplaySection = document.getElementById("user-display");
-var inputSubmitBookmark = document.getElementById("input-submit-bookmark");
-var bookmarkButtonDelete = document.getElementById("bookmark-button-delete");
 var inputWebsiteTitle = document.getElementById("input-website-title");
 var inputWebsiteUrl = document.getElementById("input-website-url");
+var inputSubmitBookmark = document.getElementById("input-submit-bookmark");
 
 // Event Listeners
 
 inputSubmitBookmark.addEventListener("click", createBookmark);
 //bookmarkButtonDelete.addEventListener("click", "REMOVE INDIVIDUAL BOOKMARK");
 
-
-// Functions
-
-function removeBookmarks() {
-  while (userDisplaySection.hasChildNodes()) {
-    userDisplaySection.removeChild(userDisplaySection.firstChild);
-}
-
-function outputBookmarks() {
-
-}
-
+//Constructor Function (Our Blueprint)
+function BookmarkCard (webTitle, webUrl, isRead) {
+  this.webTitle = webTitle;
+  this.webUrl = webUrl;
+  this.isRead = isRead;
 }
 
 function createBookmark() {
+  var sampleCard = new BookmarkCard(inputWebsiteTitle.value, inputWebsiteUrl.value, false);
+  console.log (sampleCard);
+  outputBookmarks(sampleCard);
+}
+
+
+function outputBookmarks(param1) {
+  var title = param1.webTitle;
+  var url = param1.webUrl;
 
   var newArticle = document.createElement("article");
-  var newContent = document.createTextNode("Hi there and greetings!");
+  newArticle.className = "bookmark-card";
 
-  newArticle.appendChild(newContent); //add the text node to the newly created div.
+  var newHeader = document.createElement("h3");
+  newHeader.className = "bookmark-title";
+  newHeader.innerText = title;
+  newArticle.appendChild(newHeader);
+
+  var newHr = document.createElement("hr");
+  newHr.className = "bookmark-underline";
+  newArticle.appendChild(newHr);
+
+  var newUrl = document.createElement("a");
+  newUrl.className = "bookmark-link";
+  newUrl.innerText = url;
+  newUrl.href = url;
+  newUrl.target = "_blank";
+  newArticle.appendChild(newUrl);
+
+  newArticle.appendChild(newHr.cloneNode(true));
+
+  var newReadButton = document.createElement("input");
+  newReadButton.type = "button";
+  newReadButton.name = "bookmark-read";
+  //newReadButton.id = "bookmark-button-read";
+  //aria-label
+  newReadButton.value = "Read";
+  newArticle.appendChild(newReadButton);
+
+
+  var newDeleteButton = document.createElement("input");
+  newDeleteButton.type = "button";
+  newDeleteButton.name = "bookmark-delete";
+  //newReadButton.id = "bookmark-button-read";
+  //aria-label
+  newDeleteButton.value = "Delete";
+  newArticle.appendChild(newDeleteButton);
+
+  //newArticle.appendChild(newContent); //add the text node to the newly created div.
 
   // add the newly created element and its content into the DOM
   userDisplaySection.appendChild(newArticle);
   //document.body.insertBefore(newDiv, currentDiv);
+}
 
+
+
+function removeBookmarks() {
+  while (userDisplaySection.hasChildNodes()) {
+    userDisplaySection.removeChild(userDisplaySection.firstChild);
+  }
 }
